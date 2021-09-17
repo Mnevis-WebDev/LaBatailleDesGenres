e3664c2ac199bb7d73423a9d12fae93464ac822b
Mnevis <Mnevis.webdev@gmail.com>
2021-09-17 14:26:09 -0400

    change the style with bootstrap

--- script.js
+++ script.js
@@ -1,60 +1,58 @@
 //délaration de différente liste de nom dans des tableaux.
 
 var genreDesNoms = [
-  ["photo", "f"],
-  ["tag", "m"],
-  ["ordinateur", "m"],
-  ["table", "f"]
-];
+  ['photo', 'f'],
+  ['tag', 'm'],
+  ['ordinateur', 'm'],
+  ['table', 'f'],
+]
 
 var genreDesNomsI = [
-  ["photo", "f"],
-  ["tag", "m"],
-  ["div", "f"],
-  ["table", "f"]
-];
+  ['photo', 'f'],
+  ['tag', 'm'],
+  ['div', 'f'],
+  ['table', 'f'],
+]
 
 var genreDesNomsM = [
-  ["cuisine", "f"],
-  ["four", "m"],
-  ["salle de bain", "f"],
-  ["table", "f"]
-];
+  ['cuisine', 'f'],
+  ['four', 'm'],
+  ['salle de bain', 'f'],
+  ['table', 'f'],
+]
 
 var genreDesNomsA = [
-  ["voiture", "f"],
-  ["moteur", "m"],
-  ["fenêtre", "f"],
-  ["siège", "m"]
-];
-
+  ['voiture', 'f'],
+  ['moteur', 'm'],
+  ['fenêtre', 'f'],
+  ['siège', 'm'],
+]
 
 function reponseMasculin() {
   //changement de class pour le contour de l'image
-  document.getElementsByTagName("img")[1].classList.add("border");
-  document.getElementsByTagName("img")[1].classList.add("border-dark");
+  document.getElementsByTagName('img')[1].classList.add('border')
+  document.getElementsByTagName('img')[1].classList.add('border-dark')
   //Lorsque l'utilisateur sélectionne le féminin du mot
-  if (trouverReponse(document.getElementById("question").innerHTML) == "m") {
-    alert("vous avez la bonne réponse !");
-    document.getElementById("imgReponse").src = "images/manright.gif"
+  if (trouverReponse(document.getElementById('question').innerHTML) == 'm') {
+    alert('vous avez la bonne réponse !')
+    document.getElementById('imgReponse').src = 'images/manright.gif'
   } else {
-    alert("mauvaise réponse");
-    document.getElementById("imgReponse").src = "images/manwrong.gif";
+    alert('mauvaise réponse')
+    document.getElementById('imgReponse').src = 'images/manwrong.gif'
   }
 }
 
 function reponseFeminin() {
-
   //changement de class pour le contour de l'image
-  document.getElementsByTagName("img")[3].classList.add("border");
-  document.getElementsByTagName("img")[3].classList.add("border-dark");
+  document.getElementsByTagName('img')[3].classList.add('border')
+  document.getElementsByTagName('img')[3].classList.add('border-dark')
   //Lorsque l'utilisateur sélectionne le féminin du mot
-  if (trouverReponse(document.getElementById("question").innerHTML) == "f") {
-    alert("vous avez la bonne réponse !");
-    document.getElementById("imgReponse").src = "images/womanright.gif"
+  if (trouverReponse(document.getElementById('question').innerHTML) == 'f') {
+    alert('vous avez la bonne réponse !')
+    document.getElementById('imgReponse').src = 'images/womanright.gif'
   } else {
-    alert("mauvaise réponse");
-      document.getElementById("imgReponse").src = "images/womanwrong.gif";
+    alert('mauvaise réponse')
+    document.getElementById('imgReponse').src = 'images/womanwrong.gif'
   }
 }
 
@@ -62,7 +60,7 @@ function trouverReponse(leMot) {
   //Trouvez la réponse dans la table
   for (var i = 0; i <= genreDesNoms.length; i++) {
     if (leMot == genreDesNoms[i][0]) {
-      return genreDesNoms[i][1];
+      return genreDesNoms[i][1]
     }
   }
 }
@@ -70,79 +68,87 @@ function trouverReponse(leMot) {
 function questionSuivante() {
   //verifier s'il reste des noms dans la liste
   if (genreDesNoms.length == 0) {
-    alert("Il ne reste plus de mot dans la liste. Veuilez choisir une autre liste");
-    return;
+    alert(
+      'Il ne reste plus de mot dans la liste. Veuilez choisir une autre liste'
+    )
+    return
   }
   //impression du dernier mot avec son genre
   //insererElement();
 
   //remise à zéro des contours d'images
   //changement de class pour le contour de l'image
-  document.getElementsByTagName("img")[1].classList.remove("border");
-  document.getElementsByTagName("img")[1].classList.remove("border-dark"); //changement de class pour le contour de l'image
-  document.getElementsByTagName("img")[3].classList.remove("border");
-  document.getElementsByTagName("img")[3].classList.remove("border-dark");
+  document.getElementsByTagName('img')[1].classList.remove('border')
+  document.getElementsByTagName('img')[1].classList.remove('border-dark') //changement de class pour le contour de l'image
+  document.getElementsByTagName('img')[3].classList.remove('border')
+  document.getElementsByTagName('img')[3].classList.remove('border-dark')
   //Enlever le dernier nom de la liste
-  genreDesNoms.shift();
+  genreDesNoms.shift()
 
   //verifier s'il reste des noms dans la liste
   if (genreDesNoms.length == 0) {
-    alert("Il ne reste plus de mot dans la liste. Veuilez choisir une autre liste");
-    return;
+    alert(
+      'Il ne reste plus de mot dans la liste. Veuilez choisir une autre liste'
+    )
+    return
   }
 
   //distribution aléatoire de la liste de nom
-  distributionAleatoire(genreDesNoms);
+  distributionAleatoire(genreDesNoms)
   //Lorsque l'utilisateur veut poursuivre le gif est enlevé
-  document.getElementById("imgReponse").src = "";
+  document.getElementById('imgReponse').src = ''
   //affichage de la question
-  document.getElementById("question").innerHTML = genreDesNoms[0][0];
+  document.getElementById('question').innerHTML = genreDesNoms[0][0]
 }
 
 function nombreDeMotTotal() {
   //calcul le nombre de mot
-  var i = 0;
+  var i = 0
   while (i < genreDesNoms.length) {
-    i++;
+    i++
   }
-  return i;
+  return i
 }
 
 function changerDeListe() {
-  var reponse = prompt("Quelle liste de mot voulez-vous ? (informatique(i), maison(m), automobile(a))");
-  console.log(reponse);
+  var reponse = prompt(
+    'Quelle liste de mot voulez-vous ? (informatique(i), maison(m), automobile(a))'
+  )
+  console.log(reponse)
   switch (reponse) {
-    case "i":
-      genreDesNoms = [];
-      genreDesNoms = genreDesNomsI;
-      distributionAleatoire(genreDesNoms);
-      document.getElementById("question").innerHTML = genreDesNoms[0][0];
-      break;
-    case "m":
-      genreDesNoms = [];
-      genreDesNoms = genreDesNomsM;
-      distributionAleatoire(genreDesNoms);
-      document.getElementById("question").innerHTML = genreDesNoms[0][0];
-      break;
-    case "a":
-      genreDesNoms = [];
-      genreDesNoms = genreDesNomsA;
-      distributionAleatoire(genreDesNoms);
-      document.getElementById("question").innerHTML = genreDesNoms[0][0];
-      break;
-
+    case 'i':
+      genreDesNoms = []
+      genreDesNoms = genreDesNomsI
+      distributionAleatoire(genreDesNoms)
+      document.getElementById('question').innerHTML = genreDesNoms[0][0]
+      break
+    case 'm':
+      genreDesNoms = []
+      genreDesNoms = genreDesNomsM
+      distributionAleatoire(genreDesNoms)
+      document.getElementById('question').innerHTML = genreDesNoms[0][0]
+      break
+    case 'a':
+      genreDesNoms = []
+      genreDesNoms = genreDesNomsA
+      distributionAleatoire(genreDesNoms)
+      document.getElementById('question').innerHTML = genreDesNoms[0][0]
+      break
   }
 }
 
 function pasPret() {
   //Changement du text du paragraphe
-  document.querySelector("p").innerHTML = "Une seule chose peut-être dit, si le mot fini par un 'e' il y a de bonne chance que ça soit féminin. C'est le temps de monter sur le ring !";
-  document.querySelector("p").style.color = "red";
+  document.querySelector('p').innerHTML =
+    "Une seule chose peut-être dit, si le mot fini par un 'e' il y a de bonne chance que ça soit féminin. C'est le temps de monter sur le ring !"
+  document.querySelector('.card').classList.remove('bg-secondary')
+  document.querySelector('.card').classList.add('bg-primary')
+  //document.querySelector('p').style.color = 'red'
   //effacer le bouton
-  document.getElementsByTagName("a")[1].hidden = true;
+  document.getElementsByTagName('a')[2].hidden = true
   //Change le bouton
-  document.querySelector(".btn").classList.remove("btn-primary");
-  document.querySelector(".btn").classList.add("btn-danger");
+  document.querySelector('.btn').classList.remove('btn-outline-secondary')
+  document.querySelector('.btn').classList.add('btn-outline-secondary')
 }
 
 // function insererElement() {
@@ -160,17 +166,16 @@ function pasPret() {
 // }
 
 function effacerLaListe() {
-
-  var liste = document.getElementById("liste");
-  var listItems = liste.getElementsByTagName("p");
-  var last = listItems[listItems.length - 1];
+  var liste = document.getElementById('liste')
+  var listItems = liste.getElementsByTagName('p')
+  var last = listItems[listItems.length - 1]
 
   if (listItems.length == 0) {
-    alert("Il n'y a plus de mot à effacer !");
-    return;
+    alert("Il n'y a plus de mot à effacer !")
+    return
   } else {
-    var liste = document.getElementById("liste");
-    liste.removeChild(last);
+    var liste = document.getElementById('liste')
+    liste.removeChild(last)
   }
 }
 /*
@@ -180,9 +185,9 @@ function effacerLaListe() {
  */
 function distributionAleatoire(array) {
   for (var i = array.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1));
-    var temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
+    var j = Math.floor(Math.random() * (i + 1))
+    var temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
   }
 }
